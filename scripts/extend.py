b4d2702fa7e80b36370cd057b6f0cd6dd142b80a
Attia Youssef <youssef.attia@epfl.ch>
2021-10-07 17:59:54 +0200

    poussez

---
+++ scripts/extend.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""implement a polynomial basis function."""
+
+import numpy as np
+
+
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    n = x.shape[0]
+    result = np.ones((n,1))
+    if degree == 0:
+        return result
+    else:
+        for i in range(degree + 1):
+            if i == 0:
+                continue
+            extension = np.power(x,i)
+            result = np.c_[result,extension]
+        return result
+
+    
+def extend(x,fs):
+    result = x
+    for function in fs:
+        extension = function(x)
+        result = np.c_[result,extension]
+    return result
