3b6875e4161f57b3dc7d8afc012e862e19d5dc0d
TorgemanTarak <91268951+TorgemanTarak@users.noreply.github.com>
2021-10-09 19:16:00 +0200

    addaptative step for gd

--- scripts/gradient_descent.py
+++ scripts/gradient_descent.py
@@ -27,7 +27,7 @@ def general_gradient_descent(y, tx, initial_w, max_iters, gamma, kind = 'mse'):
         
         
         
-        gamma=abs((np.dot(ws[len(ws)-1]-ws[len(ws)-2],grad-grad2))/(np.dot(grad-grad2,grad-grad2))
+        
         
         print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, gamma={gamma}".format(
               bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], gamma=gamma))
@@ -35,3 +35,26 @@ def general_gradient_descent(y, tx, initial_w, max_iters, gamma, kind = 'mse'):
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     return general_gradient_descent(y,tx,initial_w,max_iters,gamma,'mse')
+
+
+def adaptative_step_gradient_descent(y, tx, initial_w, max_iters, gamma, kind = 'mse'):
+    """Gradient descent algorithm."""
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        grad,loss = compute_gradient(y,tx,w, kind)
+        w = w - gamma * grad
+        
+        # store w and loss
+        ws.append(w)
+        
+        losses.append(loss)
+        grad2,trash =compute_gradient(y,tx,ws[len(ws)-1], kind)
+        
+        gamma=abs((np.dot(ws[len(ws)-1]-ws[len(ws)-2],grad-grad2)))/(np.dot(grad-grad2,grad-grad2))
+        
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, gamma={gamma}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], gamma=gamma))
+    return losses, ws
