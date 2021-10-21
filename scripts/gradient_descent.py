591f95c414e5f44ff57184ca1946ffd8755c1c27
TorgemanTarak <91268951+TorgemanTarak@users.noreply.github.com>
2021-10-21 16:00:22 +0200

    'MAE'

--- scripts/gradient_descent.py
+++ scripts/gradient_descent.py
@@ -57,3 +57,26 @@ def adaptative_step_gradient_descent(y, tx, initial_w, max_iters, gamma, kind = 
         print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, gamma={gamma}".format(
               bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], gamma=gamma))
     return w, loss
+
+
+### MAE
+def compute_gradient_MAE(y, tx, w):
+    e = y - np.dot(tx,w)
+    grad = (-1/tx.shape[0])*(np.dot(tx.T,np.sign(e)))
+    loss = compute_loss(y,tx,w,'mae')
+    return (grad,loss)
+
+
+
+def MAE_gradient_descent(y, tx, initial_w, max_iters, gamma ):
+    """Gradient descent algorithm."""
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        grad,loss = compute_gradient_MAE(y,tx,w)
+        w = w - gamma * grad  
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+    return (w,loss)
