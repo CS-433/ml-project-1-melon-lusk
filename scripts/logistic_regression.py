1c416b426a84ceeca74854c1024cffca8cf5b48f
Léo Dupont <45147304+Morpheo1@users.noreply.github.com>
2021-10-23 15:58:56 +0200

    Working logistic regression

--- scripts/logistic_regression.py
+++ scripts/logistic_regression.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 #Normal logistic
 def sigmoid(t):
     """apply the sigmoid function on t."""
@@ -6,20 +8,26 @@ def sigmoid(t):
 
 def calculate_loss(y, tx, w):
     """compute the loss: negative log likelihood."""
-    dot_product = np.dot(tx, w)
-    first_term = np.log( 1 + np.exp(dot_product))
-    second_term = y * dot_product
-    return np.sum(first_term - second_term, axis = 0)[0]
+    pred = sigmoid(tx @ w)
+    loss = 1/y.shape[0] * (y.T @ np.log(pred + 1e-10) + (1 - y).T @ np.log(1 - pred + 1e-10))
+    return np.squeeze(-loss)
 
 
 def calculate_gradient(y, tx, w):
     """compute the gradient of loss."""
-    return tx.T.dot(
-        sigmoid(tx.dot(w)) -y  
-    )
+    pred = sigmoid(tx @ w)
+    return 1/y.shape[0] * (tx.T @ (pred - y))
+
+def calculate_hessian(tx, w) :
+    pred = sigmoid(tx @ w)
+    test = np.einsum('ij,j->ij', tx.T, pred * (1 - pred))
+    
+    return test @ tx
+
+    #return tx.T @ (pred * (1 - pred)) @ tx
 
 
-def learning_by_gradient_descent(y, tx, w, gamma):
+def grad_loss(y, tx, w):
     """
     Do one step of gradient descent using logistic regression.
     Return the loss and the updated w.
@@ -31,20 +39,44 @@ def learning_by_gradient_descent(y, tx, w, gamma):
 
 #Penalized logistic
 
-def penalized_logistic_regression(y, tx, w, lambda_):
+def penalized_grad_loss(y, tx, w, lambda_):
     loss, grad = calculate_loss(y,tx,w), calculate_gradient(y,tx,w)
     loss += (lambda_/2) * np.power(np.linalg.norm(w),2)
     grad += lambda_ * w
     return grad, loss
 
-
-def logistic_gradient_descent(y,tx,initial_w, max_iters, gamma,lambda_):
+def penalized_logistic_regression(y,tx,initial_w, max_iters, gamma, lambda_):
     ws , losses = [initial_w], []
     w = initial_w
     for i in range(max_iters):
-        grad, loss = penalized_logistic_regression(y,tx,w,lambda_)
+        grad, loss = penalized_grad_loss(y,tx,w, lambda_)
         w = w - gamma * grad
         ws.append(w)
         losses.append(loss)
-        print("Loss at iteration ({i}/{m_i} : {loss})".format(i = i, m_i = max_iters, loss = loss))
+        print("Logistic Regression ({bi}/{ti}): loss={l}, w0={w0}, gamma={gamma}".format(
+              bi=i, ti=max_iters - 1, l=loss, w0=w[0], gamma=gamma))
+        
+        grad2 = calculate_gradient(y,tx,ws[-1])
+        
+        gamma=abs((np.dot(ws[-1]-ws[-2],grad-grad2)))/(np.dot(grad-grad2,grad-grad2))
+    return w, loss
+
+
+def logistic_regression(y,tx,initial_w, max_iters, gamma):
+    ws , losses = [initial_w], []
+    w = initial_w
+    for i in range(max_iters):
+        grad, loss = grad_loss(y,tx,w)
+        hess = calculate_hessian(tx, w)
+        w = w - gamma * np.linalg.solve(hess, grad)
+        #w = w - gamma * grad
+        ws.append(w)
+        losses.append(loss)
+        print("Logistic Regression ({bi}/{ti}): loss={l}, w0={w0}, gamma={gamma}".format(
+              bi=i, ti=max_iters - 1, l=loss, w0=w[0], gamma=gamma))
+        
+        #grad2 = calculate_gradient(y,tx,ws[-1])
+        
+        #gamma=abs((np.dot(ws[-1]-ws[-2],grad-grad2)))/(np.dot(grad-grad2,grad-grad2))
+    return w, loss
     
